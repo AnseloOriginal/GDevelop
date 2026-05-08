c5f6572335d7d0c2a3f7e11778b3b308ca952455
Uzebu Ansel <102417781+AnseloOriginal@users.noreply.github.com>
2026-05-08 10:51:45 +0100

    Add basic JSExtension.js setup

---
+++ Extensions/SceneTransition/JsExtension.js
@@ -0,0 +1,56 @@
+//@ts-check
+/// <reference path="../JsExtensionTypes.d.ts" />
+/**
+ * This is a declaration of an extension for GDevelop 5.
+ *
+ * ℹ️ Changes in this file are watched and automatically imported if the editor
+ * is running. You can also manually run `node import-GDJS-Runtime.js` (in newIDE/app/scripts).
+ *
+ * The file must be named "JsExtension.js", otherwise GDevelop won't load it.
+ * ⚠️ If you make a change and the extension is not loaded, open the developer console
+ * and search for any errors.
+ *
+ * More information on https://github.com/4ian/GDevelop/blob/master/newIDE/README-extensions.md
+ */
+
+/** @type {ExtensionModule} */
+module.exports = {
+  createExtension: function (_, gd) {
+    const extension = new gd.PlatformExtension();
+    extension
+      .setExtensionInformation(
+        'SceneTransition',
+        _('Scene Transition'),
+        _('Changes the Scene with transition effects'),
+        'Ansel Uzebu',
+        'MIT'
+      )
+      .setShortDescription(
+        'Change scenes with customizable transitionss.'
+      );
+    extension
+      .addInstructionOrExpressionGroupMetadata(_('Scene Transition'))
+      .setIcon('CppPlatform/Extensions/topdownmovementicon.png');
+
+
+       extension
+      .addAction(
+        'ChangeSceneWithTransition',
+        _('Change Scene with transition'),
+        _(
+          'Change the scene to a new scene with a transition.'
+        ),
+        _('Change Scene to _PARAM1_ using _PARAM2_ transition (Duration _PARAM3_)'),
+        _('Scene'),
+        'res/actions/saveDown.svg',
+        'res/actions/saveDown.svg'
+      )
+      .addCodeOnlyParameter('currentScene', '')
+      .addParameter('string', _('Type of Transition'), '', false)
+      .setDefaultValue(`Swipe Left`)
+      .addParameter('number', _('Duration of the transition'), '', true)
+      .setDefaultValue(`1`)
+      .getCodeExtraInformation()
+      .setFunctionName('gdjs.SceneTransition.changeSceneWithTransition');
+    }
+}
